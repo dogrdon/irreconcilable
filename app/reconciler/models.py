73c9d56d305cba19da091563367373fc371c201e
drew gordon <drew.s.gordon@gmail.com>
2015-08-11 23:56:37 -0400

    why does it remove a column from db in 0002 migration and then complain it's not there

--- app/reconciler/models.py
+++ app/reconciler/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Term(models.Model):
-	term_text = models.CharField(max_length=220)
-	add_date = models.DateTimeField('date added')
+    term_text = models.CharField(max_length=220)
+    add_date = models.DateTimeField('date added')
 
+class Uri(models.Model):
+    term = models.ForeignKey(Term)
+    uri = models.CharField(max_length=500)
+    
 
