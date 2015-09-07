ca7b3274a202548c168049e3cf537c28be000e65
drew gordon <drew.s.gordon@gmail.com>
2015-09-06 23:53:09 -0400

    no more django

--- app/reconciler/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Term
-
-class NewTermForm(forms.ModelForm):
-	class Meta:
-		model = Term
-		fields = ['term_text', 'add_date']
-
--- app/reconciler/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'(?P<term_id>[0-9]+)/$', views.term_info, name='term_info'),
-    
-]
