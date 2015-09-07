50cf1878d7f9af6ac27ec7da4d161997b57c7ae7
droquo <drew.s.gordon@gmail.com>
2015-09-07 19:37:02 -0400

    just checking in for sync

--- apps/flaskapp/app.py
+++ apps/flaskapp/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+#ROUTES
 @app.route('/')
 def index():
     return 'start here'
 
+@app.route('/terms')
+def terms():
+    return 'terms will be here'
+
+@app.route('/term/<int:term_id>')
+def term_details(term_id):
+    return 'Term %d' % term_id
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
