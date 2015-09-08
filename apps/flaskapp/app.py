129e4ebd8ae918851d1bcc8a9cf3d2b0e9a349ef
droquo <drew.s.gordon@gmail.com>
2015-09-08 01:06:44 -0400

    stopping point: settings not coming through

--- apps/flaskapp/app.py
+++ apps/flaskapp/app.py
@@ -1,10 +1,11 @@
-import sqlite3
+from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('settings')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./tmp/check.db'
+#app.config.from_object('settings')
+app.config.from_envvar('settings', silent=True)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./tmp/check.db' #this should not be here in the long run
 db = SQLAlchemy(app)
 
 #MODELS
@@ -24,6 +25,17 @@ class Uri(db.Model):
     term_id = db.Column(db.Integer, db.ForeignKey('term.id'))
     term = db.relationship('Term', backref=db.backref('terms', lazy='dynamic'))
 
+#HELPER FUNCTIONS
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.row
+    return rv
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
 #VIEWS
 @app.route('/')
 def index():
@@ -31,11 +43,17 @@ def index():
 
 @app.route('/terms')
 def terms():
-    return 'terms will be here'
+    db = get_db()
+    cur = db.execute('select term_text from terms order by id desc')
+    terms = cur.fetchall()
+    return render_template('terms.html', terms=terms)
 
 @app.route('/term/<int:term_id>')
 def term_details(term_id):
     return 'Term %d' % term_id
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
