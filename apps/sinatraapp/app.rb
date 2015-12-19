dd3cea195a1f11f2264b569e73c9e80fcdf74f5d
droquo <drew.s.gordon@gmail.com>
2015-12-18 21:37:19 -0500

    add sinatra start

--- apps/sinatraapp/app.rb
+++ apps/sinatraapp/app.rb
@@ -1,11 +1,21 @@
 require 'sinatra/base'
+require 'mongo'
+require 'json/ext'
 
 class RecApp < Sinatra::Base
     set :bind, '0.0.0.0'
 
-    get '/' do
-        "Welcome #{request.user_agent}! The time is: #{Time.now}. Pizza, Pizza!"
+    configure do
+    	db = Mongo::Client.new(['localhost:27017'], :database => 'test')
+    	set :mongo_db, db[:test]
     end
+
+    get '/' do
+        "Welcome #{request.user_agent}! The time is: #{Time.now}. Pizza, Pizza in #{settings.bind}!"
+    end
+
+    get ''
+
 
     run! if app_file == $0
 
